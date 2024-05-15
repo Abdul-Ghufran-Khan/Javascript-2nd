cf36dc5c9eabce9bbad9e4382a41943e191e0b8e
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-05-15 18:45:11 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,34 @@
+       // Question 1
+       var userName
+
+       // Question 2
+       var myName = "Abdul Ghufran"
+       var message = "Hello World"
+
+       // Question 3
+       alert(message)
+
+       // Question 4
+       var userName = prompt("Enter Your Name: ")
+       var userAge = prompt("Enter Your Age: ")
+       alert("Student Name is: " + userName)
+       alert("Student Age is: " + userAge)
+       alert("Certified Mobile Application Development")
+
+       // Question 5
+       var text = "Pizza"
+       alert(text + "\n" + text.slice(0 , 4) + "\n" + text.slice(0 , 3) + "\n" + text.slice(0 , 2) + "\n" + text.slice(0 , 1))
+       
+       // Question 6
+       var userEmail = prompt("Enter your Email: ")
+       alert("email address is " + userEmail)
+
+       // Question 7
+       var bookName = "I am trying to learn from the Book A smarter way to learn JavaScript"
+       alert(bookName)
+       
+       // Question 8
+       document.write("Yah!! I can write HTML content throught Javascript")
+       
+       // Question 9
+       alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
